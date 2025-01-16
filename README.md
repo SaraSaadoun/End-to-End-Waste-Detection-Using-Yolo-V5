386d1098538c3e7bd9fced2b5078119ab57fcd61
Sara Hamza <sarasaadoun4@gmail.com>
2025-01-17 00:53:53 +0200

    add data ingestion

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# end-to-end-waste-detection-using-yolo-v5
-End to end Waste Detection using Yolo V5
+# End to end Waste Detection using Yolo V5
+
+## Workflows
+
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
---
+++ wasteDetection/entity/config_entity.py
@@ -0,0 +1,31 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from wasteDetection.constant.training_pipeline import *
+
+
+
+@dataclass
+class TrainingPipelineConfig:
+    artifacts_dir: str = ARTIFACTS_DIR
+
+
+training_pipeline_config:TrainingPipelineConfig = TrainingPipelineConfig() 
+
+
+@dataclass
+class DataIngestionConfig:
+    # artifacts/data_ingestion -> to store zipped dataset
+    data_ingestion_dir: str = os.path.join(
+        training_pipeline_config.artifacts_dir, DATA_INGESTION_DIR_NAME
+    )
+
+    # artifacts/feature_store -> to store unzipped dataset
+    feature_store_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR
+    )
+
+    data_download_url: str = DATA_DOWNLOAD_URL
+
+
+
