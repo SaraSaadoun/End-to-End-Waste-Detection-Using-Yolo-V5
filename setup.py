ba9b721582cbcb9f8b117e5dcb3cfa28d8ab69cc
Sara Hamza <sarasaadoun4@gmail.com>
2025-01-16 20:50:50 +0200

    add project structure and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    author='Sara Hamza Saadoun',
+    author_email='sarasaadoun4@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
