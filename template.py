f6335a9d153b998af5925bf547c89dd14e1e335f
Sara Hamza <sarasaadoun4@gmail.com>
2025-01-19 06:05:08 +0200

    fix project structure

--- template.py
+++ template.py
@@ -17,8 +17,8 @@ list_of_files = [
     f'{project_name}/components/model_trainer.py',
 
     f'{project_name}/constant/training_pipeline/__init__.py',
-
-    f'{project_name}/entity/application.py',
+    f'{project_name}/constant/application.py',
+    
     f'{project_name}/entity/config_entity.py',
     f'{project_name}/entity/artifacts_entity.py',
 
